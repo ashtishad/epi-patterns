05d5f29b5352321f19501eefe6c1c0497898e34e
ashtishad <ashefhtishad@gmail.com>
2022-02-10 19:13:05 +0600

    167. Two Sum II - Input Array Is Sorted
    * Two pointer and binary search.

---
+++ twopointer/binary-search/167-two-sum-II-input-array-is-sorted/solution-167.go
@@ -0,0 +1,21 @@
+package twopointer
+
+func twoSum(nums []int, target int) []int {
+	var n = len(nums)
+	if n == 2 {
+		return []int{1, 2}
+	}
+
+	var l, r = 0, n - 1
+	for l < r {
+		var sum = nums[l] + nums[r]
+		if sum == target {
+			return []int{l + 1, r + 1} // idx are 1-based
+		} else if sum < target {
+			l++
+		} else {
+			r--
+		}
+	}
+	return nil
+}
---
+++ twopointer/binary-search/167-two-sum-II-input-array-is-sorted/solution_167_test.go
@@ -0,0 +1,26 @@
+package twopointer
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_(t *testing.T) {
+	testCases := []struct {
+		nums   []int
+		target int
+		result []int
+	}{
+		{[]int{2, 7, 11, 15}, 9, []int{1, 2}},
+		{[]int{-1, 0}, -1, []int{1, 2}},
+		{[]int{2, 3, 4}, 6, []int{1, 3}},
+	}
+
+	for _, tc := range testCases {
+		var want = tc.result
+		got := twoSum(tc.nums, tc.target)
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("failed! got: %v, want: %v, input: %v, k: %v", got, want, tc.nums, tc.target)
+		}
+	}
+}
